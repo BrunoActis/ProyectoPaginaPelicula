f1b8fa081acb710eaa2842fbde0434a850b7dd8c
Bruno Actis <brunocepas@gmail.com>
2022-08-18 11:38:55 -0300

    Primer commit

---
+++ Peliculas.js
@@ -0,0 +1,96 @@
+/*ARRAY DE PELICULAS */
+const peliculas=[
+    {
+    ID:"1",
+    titulo:"Unboxed",
+    imagen:"./unboxed-200x300.jpg",
+    genero:"terror",
+    año:"2022",
+    duracion:"1hs 13min",
+    director:"David Becerra, Eddie del Carmen",
+    actores:"Bryan Dijols, Elaine del Valle, Katherine",
+    urlpeli:"./peliculas/unboxedRef.html",
+    descripcion:"Laura Díaz es una prometedora influencer presionada por un patrocinador VIP para que ofrezca su mejor actuación en una retransmisión en directo de Halloween. Mientras la transmisión comienza con una serie de preguntas de trivia, la noche da un giro espeluznante cuando un misterioso usuario comenta sobre la vida personal de Laura. Mientras Laura sigue abriendo cajas de regalo de sus patrocinadores, pronto descubre que algunas de las cajas tienen regalos grotescos en su interior. El misterioso usuario envía entonces un mensaje de texto a Laura y le explica que tendrá que participar en su propio y enfermizo juego de regalos, o de lo contrario habrá que pagar un precio mortal"
+    },
+    {
+    ID:"2",
+    titulo:"Corazones mal heridos",
+    imagen:"./corazonesMalheridos200x300.jpg",
+    genero:"Drama",
+    año:"2022",
+    duracion:"2h 2min",
+    director:"Ellizabeth Allen,Ishai Setton",
+    actores:"Benjamín Del Cerro, A.J.Tannen, Alex Wexo, Anna Marusich",
+    urlpeli:"./peliculas/CorazonesmalheridosRef.html"
+    },
+    {
+    ID:"3",    
+    titulo:"Dc liga",
+    imagen:"./dc-liga-de-supermascotas-200x300.jpg",
+    genero:"Accion",
+    año:"2022",
+    duracion:"1hs 46min",
+    director:"Jared Stern"||"Sam Levine",
+    actores:"Alfred Molina, Amanda Manes ",
+    urlpeli:"#"
+    },
+    {
+    ID:"4",
+    titulo:"Linterna verde",
+    imagen:"./linterna-verde-200x300.jpg",
+    genero:"Animacion",
+    año:"2022",
+    duracion:"1hs 23min",
+    director:"Anna Gutto",
+    actores:"Aldis Hodge, Bryan Bloom",
+
+    },
+    {
+    ID:"5",
+    titulo:"Mi peluqueria en rio",
+    imagen:"./mi-peluqueria-en-rio-200x300.jpg",
+    genero:"Comedia",
+    año:"2022",
+    duracion:"1hs 30min",
+    director:"Leticia Prisco, Rodrigo França",
+    actores:" Bruno Jablonski, Jeniffer Dias",
+    urlpeli:"#"
+
+    },
+    {
+    ID:"6",
+    titulo:"Not okay",
+    imagen:"./not-okay-200x300.jpg",
+    genero:"Comedia",
+    año:"2022",
+    duracion:"1hs 40min",
+    director:"Quinn Shephard",
+    actores:"Blaise Corrigan, Dash Perry", 
+    urlpeli:"#"
+
+    },
+    {
+    ID:"7",
+    titulo:"Paradise highway",
+    imagen:"./paradise-highway-200x300.jpg",
+    genero:"Thriller",
+    año:"2022",
+    duracion:"1hs 55min",
+    director:"Anna Gutto",
+    actores:"Calvin Williams, Cameron Monaghan",
+    urlpeli:"#"
+
+    },
+    {
+    ID:"8",
+    titulo:"Pipa",
+    imagen:"./pipa-200x300.jpg",
+    genero:"Crimen",
+    año:"2022",
+    duracion:"1h 56m",
+    director:"Alejandro Montiel",
+    actores:"Benjamín Del Cerro, Inés Estévez, Luisana Lopilato",
+    urlpeli:"#"
+
+    },     
+]
